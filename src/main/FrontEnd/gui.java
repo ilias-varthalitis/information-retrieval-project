99bccdca884538b0b29b2c586d42e5bd4118ab5d
ilias-varthalitis <iliasvarthalitiss@gmail.com>
2024-05-19 22:17:26 +0300

    final gui

--- src/main/FrontEnd/gui.java
+++ src/main/FrontEnd/gui.java
@@ -8,19 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.store.FSDirectory;
 
 import java.io.IOException;
-import java.nio.file.Paths;
 
 import BackEnd.*;
 
@@ -35,15 +25,11 @@ public class gui extends Application {
         TextField searchField = new TextField();
         searchField.setPromptText("Enter search query");
 
-//        TextField searchByTitle = new TextField();
-//        searchField.setPromptText("Enter search title");
-
         ComboBox<String> comboBox = new ComboBox<>();
         ObservableList<String> options = FXCollections.observableArrayList(
                 "Keyword", "Author", "Title", "Year", "Abstract", "Full text"
         );
         comboBox.setItems(options);
-
 
         // Set a prompt text
         comboBox.setPromptText("Select Search By:");
@@ -53,13 +39,12 @@ public class gui extends Application {
         });
 
         Button searchButton = new Button("Search");
-        ListView<String> resultsList = new ListView<>();
-
+        ListView<VBox> resultsList = new ListView<>(); // Change the type here to ListView<VBox>
 
         searchButton.setOnAction(e -> {
             String queryString;
 
-            switch (comboBox.getSelectionModel().getSelectedItem()){
+            switch (comboBox.getSelectionModel().getSelectedItem()) {
                 case "Keyword":
                     queryString = searchField.getText();
                     break;
@@ -81,7 +66,6 @@ public class gui extends Application {
 
             if (!queryString.isEmpty()) {
                 try {
-//                    searchIndex(queryString, resultsList);
                     resultsList.getItems().clear();
 
                     Manager manager = new Manager(INDEX_DIR);
@@ -90,7 +74,23 @@ public class gui extends Application {
                     manager.search(queryString);
                     for (Document doc : manager.getSearcher().getMyList()) {
 
-                        resultsList.getItems().add("Authors: "+doc.get("authors")+"\n"+"Year: "+doc.get("year")+"\n"+"Title: "+doc.get("title")+"\n"+"Abstract: "+doc.get("abstract"));
+                        String authors = doc.get("authors");
+                        String year = doc.get("year");
+                        String title = doc.get("title");
+                        String abstractContent = doc.get("abstract");
+                        String fullTextContent = doc.get("full_text");
+
+                        Label authorsLabel = new Label("Authors: " + authors);
+                        Label yearLabel = new Label("Year: " + year);
+                        Label titleLabel = new Label("Title: " + title);
+                        Hyperlink abstractLink = new Hyperlink("Abstract");
+                        Hyperlink fullTextLink = new Hyperlink("Full text");
+
+                        abstractLink.setOnAction(event -> openAbstractWindow(abstractContent));
+                        fullTextLink.setOnAction(event -> openFullTextWindow(fullTextContent));
+
+                        VBox vBox = new VBox(5, authorsLabel, yearLabel, titleLabel, abstractLink, fullTextLink);
+                        resultsList.getItems().add(vBox);
                     }
 
                     manager.deleteIndexes();
@@ -106,7 +106,7 @@ public class gui extends Application {
         VBox vboxDroplist = new VBox(10, comboBox);
         vboxDroplist.setPadding(new Insets(10));
 
-        vbox.getChildren().addAll(vboxDroplist,searchField, searchButton, resultsList); //new Label("Lucene Search"),
+        vbox.getChildren().addAll(vboxDroplist, searchField, searchButton, resultsList);
 
         Scene scene = new Scene(vbox, 700, 400);
 
@@ -114,6 +114,51 @@ public class gui extends Application {
         primaryStage.show();
     }
 
+    private void openAbstractWindow(String abstractContent) {
+        Stage abstractStage = new Stage();
+        abstractStage.setTitle("Abstract");
+
+        Label abstractLabel = new Label(abstractContent);
+        abstractLabel.setWrapText(true);
+
+        ScrollPane scrollPane = new ScrollPane(abstractLabel);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPrefSize(600, 400);
+
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setOnAction(e -> abstractStage.close());
+
+        VBox vbox = new VBox(10, scrollPane, cancelButton);
+        vbox.setPadding(new Insets(10));
+
+        Scene scene = new Scene(vbox, 700, 600); // Increase the size of the window
+        abstractStage.setScene(scene);
+        abstractStage.setResizable(true);
+        abstractStage.show();
+    }
+
+    private void openFullTextWindow(String fullTextContent) {
+        Stage fullTextStage = new Stage();
+        fullTextStage.setTitle("Full Text");
+
+        Label fullTextLabel = new Label(fullTextContent);
+        fullTextLabel.setWrapText(true);
+
+        ScrollPane scrollPane = new ScrollPane(fullTextLabel);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPrefSize(600, 400);
+
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setOnAction(e -> fullTextStage.close());
+
+        VBox vbox = new VBox(10, scrollPane, cancelButton);
+        vbox.setPadding(new Insets(10));
+
+        Scene scene = new Scene(vbox, 700, 600); // Increase the size of the window
+        fullTextStage.setScene(scene);
+        fullTextStage.setResizable(true);
+        fullTextStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
