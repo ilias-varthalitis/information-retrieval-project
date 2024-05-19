400eb4d636d4c9854122f584064c50f1945de885
arpng <aris3b@gmail.com>
2024-05-19 21:15:30 +0300

    added search by keyword and field in GUI

--- src/main/FrontEnd/gui.java
+++ src/main/FrontEnd/gui.java
@@ -1,12 +1,11 @@
 package FrontEnd;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -36,18 +35,56 @@ public class gui extends Application {
         TextField searchField = new TextField();
         searchField.setPromptText("Enter search query");
 
+//        TextField searchByTitle = new TextField();
+//        searchField.setPromptText("Enter search title");
+
+        ComboBox<String> comboBox = new ComboBox<>();
+        ObservableList<String> options = FXCollections.observableArrayList(
+                "Keyword", "Author", "Title", "Year", "Abstract", "Full text"
+        );
+        comboBox.setItems(options);
+
+
+        // Set a prompt text
+        comboBox.setPromptText("Select Search By:");
+        comboBox.setOnAction(event -> {
+            String selectedOption = comboBox.getSelectionModel().getSelectedItem();
+            System.out.println("Selected: " + selectedOption);
+        });
+
         Button searchButton = new Button("Search");
         ListView<String> resultsList = new ListView<>();
 
 
-        Manager manager = new Manager(INDEX_DIR);
-
-
         searchButton.setOnAction(e -> {
-            String queryString = searchField.getText();
+            String queryString;
+
+            switch (comboBox.getSelectionModel().getSelectedItem()){
+                case "Keyword":
+                    queryString = searchField.getText();
+                    break;
+                case "Author":
+                    queryString = "author:" + searchField.getText();
+                    break;
+                case "Title":
+                    queryString = "title:" + searchField.getText();
+                    break;
+                case "Year":
+                    queryString = "year:" + searchField.getText();
+                    break;
+                case "Abstract":
+                    queryString = "abstract:" + searchField.getText();
+                    break;
+                default: // "Full text":
+                    queryString = "fulltext:" + searchField.getText();
+            }
+
             if (!queryString.isEmpty()) {
                 try {
 //                    searchIndex(queryString, resultsList);
+                    resultsList.getItems().clear();
+
+                    Manager manager = new Manager(INDEX_DIR);
                     manager.indexDocuments();
 
                     manager.search(queryString);
@@ -65,9 +102,14 @@ public class gui extends Application {
 
         VBox vbox = new VBox(10);
         vbox.setPadding(new Insets(10));
-        vbox.getChildren().addAll(new Label("Lucene Search"), searchField, searchButton, resultsList);
 
-        Scene scene = new Scene(vbox, 700, 600);
+        VBox vboxDroplist = new VBox(10, comboBox);
+        vboxDroplist.setPadding(new Insets(10));
+
+        vbox.getChildren().addAll(vboxDroplist,searchField, searchButton, resultsList); //new Label("Lucene Search"),
+
+        Scene scene = new Scene(vbox, 700, 400);
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
